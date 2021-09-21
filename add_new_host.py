0f2b6f88a49f70eca284361fe7124c8003e22103
ismayilm <essesn23@gmail.com>
2021-09-21 16:54:13 +0300

    Add new customer function

--- add_new_host.py
+++ add_new_host.py
@@ -35,6 +35,7 @@ def add_host():
     new_user = get_new_user_credentials()
     connection.sftp_password_operation(new_user.ip, new_user.username, new_user.password)
     storm.add_new_host(new_user.alias, new_user.ip, new_user.username, new_user.port)
+    host_file_operations.add_new_host(new_user.ip, new_user.username)
     authorized_keys.sync(private_key)
     storm.sync(private_key)
     helper.sync_files_with_hosts(private_key)
